3f10874dfed84377f771618e654a280734ad6e38
Cyrus Buhariwalla <cyrusb01@outlook.com>
2021-11-24 13:28:42 -0600

    changed layout and added solana

--- app.py
+++ app.py
@@ -4,7 +4,31 @@ import pandas as pd
 import yfinance as yf 
 import quantstats as qs
 import plotly
+from datetime import datetime
 
+def get_data(ticker):
+    
+    if ticker[0:10] != "Blockforce":
+        print(ticker)
+        now = datetime.now()
+        now_str = now.strftime('%Y-%m-%d')
+        print(now_str)
+        ticker_data = yf.download(ticker, start="2016-01-01", end=now_str)
+        ticker_data = pd.DataFrame(ticker_data["Close"])
+        ticker_data = ticker_data.rename(columns={"Close": ticker})
+        new_ticker = ticker
+    else:
+        strat = ticker
+        strat = strat.replace(" ", "_")
+        ticker_data = pd.read_csv(strat+".csv")
+        ticker_data = ticker_data.set_index("datetime")
+        ticker_data.index = [x[:10] for x in ticker_data.index]
+        ticker_data.index = pd.to_datetime(ticker_data.index)
+        ticker_data = pd.DataFrame(ticker_data["Strategy"])
+        new_ticker = ticker[:10] + ticker[25:]
+        ticker_data = ticker_data.rename(columns={"Strategy": new_ticker})
+        
+    return [ticker_data, new_ticker]
 qs.extend_pandas()
 
 
@@ -15,79 +39,59 @@ st.set_page_config(layout = "wide")
 
 
 
-cryptos = ["BTC-USD", "ETH-USD", "BNB-USD", "XRP-USD", "ADA-USD"]
+cryptos = ["BTC-USD", "ETH-USD", "BNB-USD", "XRP-USD", "ADA-USD", "SOL1-USD"]
 indexes = ["SPY", "QQQ"]
-strategies = ["Stacked BTC", "Stacked BTC Fast", "Stacked ETH Hyper"]
+strategies = ["Blockforce Alpha Predator BTC Daily", "Blockforce Alpha Predator BTC Fast", "Blockforce Alpha Predator ETH Hyper"]
 
 options = strategies + cryptos + indexes
-col1, col2, col3 = st.columns((1, 3, 1))
-col2.title("Technical Analysis Dashboard")
+a, title, b = st.columns((1, 4, 1))
+a, input1, input2, b = st.columns((1, 2, 2, 1))
+col1, col2, col3 = st.columns((1, 5, 1))
+a, fig1, fig2, b= st.columns((1, 3, 3, 1))
+a, fig3, fig4, b= st.columns((1, 3, 3, 1))
+a, stats, b = st.columns((1, 4, 1))
 
-ticker = col2.selectbox("Choose Ticker", options)
-def get_data(ticker):
-    
-    if ticker[0:7] != "Stacked":
-        print(ticker)
-        ticker_data = yf.download(ticker, start="2016-01-01", end="2021-11-17")
-        ticker_data = pd.DataFrame(ticker_data["Close"])
-        ticker_data = ticker_data.rename(columns={"Close": ticker})
-    else:
-        strat = ticker
-        strat = strat.replace(" ", "_")
-        ticker_data = pd.read_csv(strat+".csv")
-        ticker_data = ticker_data.set_index("datetime")
-        ticker_data.index = [x[:10] for x in ticker_data.index]
-        ticker_data.index = pd.to_datetime(ticker_data.index)
-        ticker_data = pd.DataFrame(ticker_data["Strategy"])
-        ticker_data = ticker_data.rename(columns={"Strategy": ticker})
-        
-    return ticker_data
+
+title.title("Quantitative Strategy and Asset Dashboard")
+
+ticker = input1.selectbox("Choose Ticker", options)
+index_choice = input2.selectbox("Choose Benchmark", options, index = 3)
+
 
 
 
 ######################## Format Ticker Data ##########################
-ticker_data = get_data(ticker)
+ticker_data, ticker = get_data(ticker)
 print(ticker_data)
 ticker_qs = ticker_data.pct_change()
-
-
-################################# Graph Ticker Data ##############################
-col2.subheader(ticker +" Price Chart")
-col2.line_chart(ticker_data, height = 500)
-
-
-
-
 ########################## Format Index Data ########################
-# index_data = yf.download(indexes, start="2016-01-01", end="2021-11-17")
-# index_options = options.copy()
-# index_options.remove(ticker)
-index_choice = col2.selectbox("Choose Benchmark", options, index = 3)
-
 if index_choice == ticker:
     index_choice = "SPY"
 
-index_choice_data = get_data(index_choice)
+index_choice_data, index_choice = get_data(index_choice)
 print(index_choice_data)
-# index_choice_data = pd.DataFrame(index_data["Close"][index_choice])
 index_qs = index_choice_data.pct_change()
 
 
 
+########################## Formatting Data ##############################
+combined_data = pd.DataFrame(ticker_qs).join(pd.DataFrame(index_qs), how = "outer")
+combined_data = combined_data.dropna()
+
+ticker_qs = combined_data[ticker]
+index_qs = combined_data[index_choice]
 
 
 
-
-
-ticker_qs = ticker_qs.dropna()
-ticker_qs = ticker_qs.squeeze()
-
-index_qs = index_qs.dropna()
-index_qs = index_qs.squeeze()
-
-
+################################# Graph Ticker Data ##############################
+# col2.subheader(ticker +" Price Chart")
+# col2.line_chart(ticker_data, height = 500)
 
 ######################## Graph Combined Data #########################
+
+
+
+
 
 ticker_port = pd.DataFrame(qs.utils.make_portfolio(ticker_qs, start_balance = 100))
 ticker_port.columns = [ticker]
@@ -98,7 +102,7 @@ combined_data = ticker_port.join(index_port, how = "outer")
 combined_data = combined_data.dropna()
 
 col2.subheader("Compare " + ticker + " to an benchmark")
-col2.line_chart(combined_data, height = 500)
+col2.line_chart(combined_data, width = 1150, height = 500, use_container_width = False)
 
 ################## Rolling Sharpe ###########################
 ticker_sharpe = pd.DataFrame(qs.stats.rolling_sharpe(ticker_qs, 0., 182, True, 365))
@@ -107,8 +111,8 @@ index_sharpe = pd.DataFrame(qs.stats.rolling_sharpe(index_qs, 0., 126, True, 252
 rolling_sharpe = ticker_sharpe.join(index_sharpe, how="outer")
 rolling_sharpe = rolling_sharpe.dropna()
 
-col2.subheader("Rolling Sharpe Ratios")
-col2.line_chart(rolling_sharpe, height = 500)
+fig1.subheader("Rolling Sharpe Ratios")
+fig1.line_chart(rolling_sharpe, width = 600, height = 300, use_container_width = False)
 
 
 ################## Drawdown Chart ###########################
@@ -118,8 +122,8 @@ index_drawdown = pd.DataFrame(qs.stats.to_drawdown_series(index_qs))
 drawdown = ticker_drawdown.join(index_drawdown, how="outer")
 drawdown = drawdown.dropna()
 
-col2.subheader("Drawdown Chart")
-col2.line_chart(drawdown, height = 500)
+fig2.subheader("Drawdown Chart")
+fig2.line_chart(drawdown, width = 600, height = 300, use_container_width = False)
 
 ################## Volatility Chart ###########################
 ticker_volatility = pd.DataFrame(qs.stats.rolling_volatility(ticker_qs, 182, 365))
@@ -128,15 +132,15 @@ index_volatility = pd.DataFrame(qs.stats.rolling_volatility(index_qs))
 rolling_volatility = ticker_volatility.join(index_volatility, how="outer")
 rolling_volatility = rolling_volatility.dropna()
 
-col2.subheader("6 Month Rolling Volatility")
-col2.line_chart(rolling_volatility, height = 500)
+fig3.subheader("6 Month Rolling Volatility")
+fig3.line_chart(rolling_volatility, width = 600, height = 300, use_container_width = False)
 
 
 
 ##################### Heatmap ##############################
 fig = ticker_qs.plot_monthly_heatmap(show= False)
-col2.subheader("Monthly Returns Heatmap for " + ticker)
-col2.pyplot(fig, height = 500)
+fig4.subheader("Monthly Returns Heatmap")
+fig4.pyplot(fig, width = 600, height = 300, use_container_width = False)
 
 
 
@@ -152,4 +156,4 @@ for stat in bad_formatted:
 metrics.columns = [ticker, index_choice]
 
 metrics_str = metrics.astype(str)
-col2.dataframe(metrics_str, width = 700, height = 1200)
+stats.dataframe(metrics_str, width = 700, height = 1200)
